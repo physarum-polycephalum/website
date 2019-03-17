35b047ae7c633635d57d579820757843be5de353
AnanaGame <gameanana@gmail.com>
2019-03-17 12:03:21 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,449 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="nav-container">
+      <div class="bar bar--sm visible-xs">
+        <div class="container">
+          <div class="row">
+            <div class="col-9 col-md-10 text-right">
+              <a href="#" class="hamburger-toggle" data-toggle-class="#menu1;hidden-xs">
+                <i class="icon icon--sm stack-interface stack-menu"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav id="menu1" class="bar bar--sm bar-1 hidden-xs bar--absolute bar--transparent">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-1 col-md-2 hidden-xs">
+              <h1 class="color--primary">TPE</h1>
+            </div>
+            <div class="col-lg-11 col-md-12 text-right text-left-xs text-left-sm">
+              <div class="bar__module">
+                <a class="btn btn--sm inner-link type--uppercase" href="#histoire-et-fonctionnement">
+                  <span class="btn__text">
+                    Histoire et fonctionnement
+                  </span>
+                </a>
+                <a class="btn btn--sm inner-link type--uppercase" href="#experimentations">
+                  <span class="btn__text">
+                    Expérimentations
+                  </span>
+                </a>
+                <a class="btn btn--sm inner-link type--uppercase" href="#modelisation">
+                  <span class="btn__text">
+                    Modélisation
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="main-container">
+      <section class="cover height-100 imagebg parallax" data-overlay="4">
+        <div class="background-image-holder">
+          <img alt="background" src="img/background.jpg" />
+        </div>
+        <div class="container pos-vertical-center">
+          <div class="row">
+            <div class="col-md-9 col-lg-7">
+              <div class="typed-headline">
+                <span class="h1 typed-text typed-text--cursor" data-typed-strings="Un être hors du commun : Le physarum polycephalum"></span>
+              </div>
+              <p class="lead">
+                Est-il possible modéliser le comportement d'un être vivant sous forme informatique ?
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="switchable feature-large">
+        <div class="container">
+          <div class="row justify-content-around">
+            <div class="col-md-6">
+              <img alt="Image" class="border--round" src="img/education-2.jpg" />
+            </div>
+            <div class="col-md-6 col-lg-5">
+              <div class="switchable__text">
+                <h2>A unique approach to learning for tertiary education</h2>
+                <p class="lead">
+                  Launching an attractive and scalable website quickly and affordably is important for modern startups &mdash; Stack offers massive value without looking 'bargain-bin'.
+                </p>
+                <a href="#">Learn More &raquo;</a>
+              </div>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="text-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+              <h2>Find your course, and your calling.</h2>
+              <p class="lead">
+                Our vision is for strong and resilient students where all have adequate access to the support structures required to succeed. Browse courses below to find out more.
+              </p>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <form class="row">
+                <div class="col-md-6">
+                  <input type="text" name="keywords" placeholder="Keywords" />
+                </div>
+                <div class="col-md-4">
+                  <div class="input-select">
+                    <select>
+                      <option selected="" value="Default">Select An Option</option>
+                      <option value="option1">Option 1</option>
+                      <option value="option2">Option 2</option>
+                      <option value="option3">Option 3</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <button type="submit" class="btn btn--primary type--uppercase">Search</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Mechanical Engineering</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Media &amp; Communications</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Biomedical Engineering</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Psychological Sciences</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Masters of Teaching</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+            <div class="col-md-4">
+              <div class="feature feature-1 boxed boxed--border">
+                <h5>Masters of Psychology</h5>
+                <p>
+                  Following BEM conventions for readability
+                </p>
+                <a href="#">
+                  Learn More
+                </a>
+              </div>
+              <!--end feature-->
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="imagebg parallax" data-scrim-top="9">
+        <div class="background-image-holder">
+          <img alt="background" src="img/education-3.jpg" />
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="cta">
+                <h2>Applications are now open</h2>
+                <p class="lead">
+                  We're now taking enrolments for Semester 1 &mdash; 2017. Stack graduates are 40% more likely to find ongoing work in their chosen profession.
+                </p>
+                <a class="btn btn--primary type--uppercase" href="https://themeforest.net/item/stack-multipurpose-html-with-page-builder/19337626?ref=medium_rare">
+                                    <span class="btn__text">
+                                        Apply Now for 2017
+                                    </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="text-center bg--secondary">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+              <h2>What makes us different?</h2>
+              <p class="lead">
+                Our vision is for strong and resilient students where all have adequate access to the support structures required to succeed. Browse features below to find out more.
+              </p>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="switchable bg--secondary">
+        <div class="container">
+          <div class="row justify-content-around">
+            <div class="col-md-6">
+              <ul class="accordion accordion--oneopen accordion-2 switchable__text">
+                <li class="active">
+                  <div class="accordion__title">
+                    <span class="h5">Campus Life</span>
+                  </div>
+                  <div class="accordion__content">
+                    <p class="lead">
+                      Stack follows the BEM naming convention that focusses on logical code readability that is reflected in both the HTML and CSS. Interactive elements such as accordions and tabs follow the same markup patterns making rapid development easier for developers and beginners alike.
+                    </p>
+                    <p class="lead">
+                      Add to this the thoughtfully presented documentation, featuring code highlighting, snippets, class customizer explanation and you've got yourself one powerful value package.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="accordion__title">
+                    <span class="h5">Study Support</span>
+                  </div>
+                  <div class="accordion__content">
+                    <p class="lead">
+                      Stack offers a clean and contemporary to suit a range of purposes from corporate, tech startup, marketing site to digital storefront. Elements have been designed to showcase content in a diverse yet consistent manner.
+                    </p>
+                    <p class="lead">
+                      Multiple font and colour scheme options mean that dramatically altering the look of your site is just clicks away &mdash; Customizing your site in the included Variant Page Builder makes experimenting with styles and content arrangements dead simple.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="accordion__title">
+                    <span class="h5">Qualification</span>
+                  </div>
+                  <div class="accordion__content">
+                    <p class="lead">
+                      Medium Rare is an elite author known for offering high-quality, high-value products backed by timely and personable support. Recognised and awarded by Envato on multiple occasions for producing consistently outstanding products, it's no wonder over 20,000 customers enjoy using Medium Rare templates.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+              <!--end accordion-->
+            </div>
+            <div class="col-lg-5 col-md-6">
+              <img alt="Image" class="border--round box-shadow-wide" src="img/education-4.jpg" />
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <section class="unpad">
+        <div class="row--gapless row">
+          <div class="col-md-4 col-6">
+            <a href="#" class="block">
+              <div class="feature feature-7 boxed text-center imagebg" data-overlay="3">
+                <div class="background-image-holder">
+                  <img alt="background" src="img/education-5.jpg" />
+                </div>
+                <h4 class="pos-vertical-center">Future Students</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 col-6">
+            <a href="#" class="block">
+              <div class="feature feature-7 boxed text-center imagebg" data-overlay="5">
+                <div class="background-image-holder">
+                  <img alt="background" src="img/education-6.jpg" />
+                </div>
+                <h4 class="pos-vertical-center">Current Students</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 col-6">
+            <a href="#" class="block">
+              <div class="feature feature-7 boxed text-center imagebg" data-overlay="5">
+                <div class="background-image-holder">
+                  <img alt="background" src="img/education-7.jpg" />
+                </div>
+                <h4 class="pos-vertical-center">Alumni</h4>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
+      <footer class="space--sm footer-2 ">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-lg-3 col-6">
+              <h6 class="type--uppercase">Company</h6>
+              <ul class="list--hover">
+                <li>
+                  <a href="#">About Company</a>
+                </li>
+                <li>
+                  <a href="#">Our Team</a>
+                </li>
+                <li>
+                  <a href="#">Locations</a>
+                </li>
+                <li>
+                  <a href="#">History</a>
+                </li>
+                <li>
+                  <a href="#">Work With Us</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-3 col-6">
+              <h6 class="type--uppercase">Developers</h6>
+              <ul class="list--hover">
+                <li>
+                  <a href="#">Developer Center</a>
+                </li>
+                <li>
+                  <a href="#">API Reference</a>
+                </li>
+                <li>
+                  <a href="#">Downloads</a>
+                </li>
+                <li>
+                  <a href="#">Tools</a>
+                </li>
+                <li>
+                  <a href="#">Developer Blog</a>
+                </li>
+                <li>
+                  <a href="#">Developer Forums</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-3 col-6">
+              <h6 class="type--uppercase">Support</h6>
+              <ul class="list--hover">
+                <li>
+                  <a href="#">Help Center</a>
+                </li>
+                <li>
+                  <a href="#">Live Chat</a>
+                </li>
+                <li>
+                  <a href="#">Downloads</a>
+                </li>
+                <li>
+                  <a href="#">Press Kit</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-3 col-6">
+              <h6 class="type--uppercase">Locations</h6>
+              <ul class="list--hover">
+                <li>
+                  <a href="#">Melbourne</a>
+                </li>
+                <li>
+                  <a href="#">London</a>
+                </li>
+                <li>
+                  <a href="#">New York</a>
+                </li>
+                <li>
+                  <a href="#">San Francisco</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!--end of row-->
+          <div class="row">
+            <div class="col-md-6">
+                            <span class="type--fine-print">&copy;
+                                <span class="update-year"></span> Stack Inc.</span>
+              <a class="type--fine-print" href="#">Privacy Policy</a>
+              <a class="type--fine-print" href="#">Legal</a>
+            </div>
+            <div class="col-md-6 text-right text-left-xs">
+              <ul class="social-list list-inline list--hover">
+                <li>
+                  <a href="#">
+                    <i class="socicon socicon-google icon icon--xs"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="socicon socicon-twitter icon icon--xs"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="socicon socicon-facebook icon icon--xs"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="socicon socicon-instagram icon icon--xs"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </footer>
+    </div>
   </div>
 </template>
 
